332d1b76776ff5f888c19bbc3f3d8b9b59bb4762
Chuanping ZHUANG <zcphoenix@outlook.com>
2019-10-09 17:16:31 +0800

    boost::algorithm::trim&split, boost::format

--- boost_study.cpp
+++ boost_study.cpp
@@ -2,19 +2,24 @@
 //
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <boost/format.hpp>
+#include <boost/algorithm/string.hpp>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string str = " zcp, , phoenix,  1234 ";
+    cout << str << endl;
+
+    vector<string> splitVec;
+    boost::algorithm::split(splitVec, str, boost::algorithm::is_any_of(","));
+    for (auto iter = splitVec.begin(); iter != splitVec.end(); iter++) {
+        boost::algorithm::trim(*iter);
+        cout << *iter << "/" << endl;
+    }
+
+    cout << endl << boost::format("writing %1%, x = %2% : %3%-th try %1%") % "toto" % 40.23 % 50 << endl;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
